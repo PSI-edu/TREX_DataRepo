3510e2b0467f8dddbf51651b1498db34c7724fb7
Pamela Gay <pamela@starstryder.com>
2023-12-20 17:22:39 -0600

    Software to list out all the data in trex

---
+++ src/display.php
@@ -0,0 +1,20 @@
+<?php
+// This is used by the simple viewer and Wordpress to display things
+?>
+<h2>Data Available</h2>
+<?php
+// List out all the directories in the data directory one level up, and all the files in each directory
+$dir = "../data";
+$files = scandir($dir);
+foreach ($files as $file) {
+    if ($file != "." && $file != "..") {
+        echo "<h3>$file</h3>";
+        $subdir = "$dir/$file";
+        $subfiles = scandir($subdir);
+        foreach ($subfiles as $subfile) {
+            if ($subfile != "." && $subfile != "..") {
+                echo "<a href=\"../data/$file/$subfile\">$subfile</a><br>";
+            }
+        }
+    }
+}
---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+/* Simple Viewer */
+
+?>
+<html>
+<head>
+<title>Simple Viewer</title>
+</head>
+<body>
+<h3>Simple Viewer</h3>
+<?php
+include 'display.php';
+?>
+</body>
+</html>
+
